712b813997a3b1b27e1e7c07969b2a1ac400ae4b
seanw <saworley39@yahoo.com>
2021-11-25 16:57:49 -0800

    Reverted the change to the LCD timings because it turned out all I had to do was disconnect and reconnect the power.

--- jukebox/__init__.py
+++ jukebox/__init__.py
@@ -28,8 +28,8 @@ class LCD:
         self.d5 = d5
         self.d6 = d6
         self.d7 = d7
+        self._enable_delay = 0.0000001
 
-        self._enable_delay = 0.001
         self._lcd_write(bytes([
             0x33,
             0x32,
@@ -38,8 +38,6 @@ class LCD:
             0b00101000,  # init
             0b00000001,  # clear.
         ]), False)
-        #self._enable_delay = 0.0000001
-        self._enable_delay = 0.0001
 
         gpio.setup(bl_red, gpio.OUT)
         gpio.setup(bl_green, gpio.OUT)
