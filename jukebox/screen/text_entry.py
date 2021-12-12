7c5fe9976d300432a471a759b279498a277c76f5
seanw <saworley39@yahoo.com>
2021-12-12 14:20:48 -0800

    Fixed some bugs and made some tweaks to the text entry after using it a bit.  Reduced the encoder sensitivity from half ticks to full ticks as stopping the encoder on a half tick proved finicky, and made it so that moving over a character you've already entered and turning the knob starts on that character rather than the last character you entered.

--- jukebox/screen/text_entry.py
+++ jukebox/screen/text_entry.py
@@ -4,18 +4,34 @@ from .. import Display
 
 CHARACTERS = b' abcdefghijklmnopqrstuvwxyz'
 
+# NOTE: throughout this file, I use the term "charsets" to refer to "subsets of ASCII which the user can cycle through".
+# Examples of charsets might be "uppercase letters", "lowercase letters", and "numbers/special symbols".
+# I'm not referring to "charsets" as in ANSI codepages.  HD44780 displays don't support those anyway.
+
+# If the user wanted to enter the string "I'm 5", for example, they would switch to the uppercase charset, select I,
+# switch to the symbols charset, select apostrophe, switch to the lowercase charset, select M, then switch to the
+# numbers charset and select 5.
+
+# Currently, if you go off the end of a charset, you go back to its beginning rather than the beginning of the next
+# charset, so charsets are effectively each their own self-contained loops rather than being contigouous.
+# I can (and probably should) make this a config option.
+
 class TextInputScreen(BaseScreen):
     disallow_popups = True
     def __init__(self, display: Display, next_screen, cancel_screen, charsets=(b' abcdefghijklmnopqrstuvwxyz',)):
         super().__init__()
         self.display = display
-        self.next_screen = self
+        self.next_screen = next_screen
         self.cancel_screen = cancel_screen
         self.entered_text = bytearray()
         self.display_offset = 0
         self.absolute_offset = 0
         self.charsets = charsets
-        self.selected_charset = 0
+        # selected_charset: the last charset the user cycled to by pressing the repeat button, which no code except
+        #                   the user input handler changes
+        # detected_charset: the charset containing the character under the cursor, which is ephemeral and changes
+        #                   every time the cursor moves
+        self.selected_charset = self.detected_charset = 0
         self.current_character = 0
         self.last_encoder_position = 0
 
@@ -23,13 +39,16 @@ class TextInputScreen(BaseScreen):
         self.display.reset_rotary_encoder()
         self.scroll(0, True)
 
-    @on_encoder_tick(2)
+    @on_encoder_tick(4)
     def on_encoder_tick(self, n):
-        if not self.display.encoder_pressed:
+        if self.display.encoder_pressed:
+            self.scroll(n)
+        else:
             self.cycle(n)
 
+    @on_button_pressed(Buttons.ENCODER)
     @on_button_pressed(Buttons.NEXT)
-    def scroll_forward(self):
+    def scroll_right(self):
         self.scroll(1)
 
     @on_button_pressed(Buttons.PREVIOUS)
@@ -38,11 +57,12 @@ class TextInputScreen(BaseScreen):
 
     @on_button_pressed(Buttons.REPEAT)
     def switch_charsets(self):
-        self.selected_charset = (self.selected_charset + 1) % len(self.charsets)
+        # cycle through starting on the current charset being used (the detected charset)
+        self.selected_charset = self.detected_charset = (self.detected_charset + 1) % len(self.charsets)
         self.cycle(0)
 
     def cycle(self, n):
-        charset = self.charsets[self.selected_charset]
+        charset = self.charsets[self.detected_charset]
         charidx = (self.current_character + n) % len(charset)
         if n != 0:
             # don't bother writing back the result of the modulus if we're just cycling through charsets,
@@ -57,29 +77,70 @@ class TextInputScreen(BaseScreen):
         self.display.lcd._lcd_write(0xC0 | offset, False)
 
     def scroll(self, n, force_redraw=False):
-        offset = self.absolute_offset = self.absolute_offset + n
+        offset = self.absolute_offset = max(0, self.absolute_offset + n)
         if len(self.entered_text) <= self.absolute_offset:
             charset = self.charsets[self.selected_charset]
-            character = charset[self.current_character%len(charset)]
+            character = charset[self.current_character % len(charset)]
             self.entered_text.extend(character for _ in range(self.absolute_offset - len(self.entered_text) + 1))
             force_redraw = True
+            # we're moving into a brand new cell, use the last selected charset.
+            # self.detected_charset = self.selected_charset
+        else:
+            pos = self.charsets[self.selected_charset].find(self.entered_text[self.absolute_offset])
+            if pos != -1:
+                self.detected_charset = self.selected_charset
+                self.current_character = pos
+            else:
+                # XXX what should be the behavior here?
+                # the situation here is that the user has moved the cursor from a position with one charset to
+                # a position with another, i.e., after editing a lowercase letter, moved the cursor to a position
+                # that already contained an uppercase one.
+                # What I've opted to do is maintain two state variables: selected_charset and detected_charset.
+                # selected_charset is the last charset the user actually selected by pressing Repeat to cycle through
+                # them, which is the first one searched when moving to a new cell (important if multiple charsets
+                # contain the same character, such as space), and which is automatically switched to when
+                # moving to a new cell.  This second behavior may be confusing, and I may remove it depending on
+                # how it feels to use.
+                # The second state variable, detected_charset, is the one turning the rotary encoder actually picks
+                # characters from, which is automatically switched out based on which character is under the cursor.
+                character = self.entered_text[self.absolute_offset]
+                for i, charset in enumerate(self.charsets):
+                    pos = charset.find(character)
+                    if pos != -1:
+                        self.detected_charset = i
+                        self.current_character = pos
+                        break
+                else:
+                    # If we get here, a character not in any of our charsets got into self.entered_text somehow.
+                    # Since the user can only select characters from one of the charsets, the only way that can happen
+                    # is if another screen put it there.
+                    # What should be the behavior here?  I don't want to crash.
+                    # I guess I'll just leave the state variables where they are.
+                    pass
         screen_cursor_pos = self.absolute_offset - self.display_offset
-        if screen_cursor_pos < 3 and offset > 3:
-            self.display_offset -= 1
+        # first check absolute bounds
+        if screen_cursor_pos <= 0:
+            screen_cursor_pos = 0
+        elif screen_cursor_pos >= 16:
+            screen_cursor_pos = 15
+        # then do some nice scrolling effects when the cursor gets near the edge of the screen
+        elif screen_cursor_pos < 3 and offset >= 3:
             screen_cursor_pos = 3
-            force_redraw = True
         elif screen_cursor_pos > 13 and offset < len(self.entered_text) - 3:
-            self.display_offset += 1
             screen_cursor_pos = 13
+        # if we don't have to scroll, and we weren't forced to redraw...
         elif not force_redraw:
             # set cursor position to the second line at the given offset.
             self.display.lcd._lcd_write(0xC0 | screen_cursor_pos, False)
             return
-        # TODO turn cursor blink off during text update, then back on after.
-        self.display.lcd._lcd_write(0b1000,False)
+        # recompute the display offset
+        self.display_offset = self.absolute_offset - screen_cursor_pos
+        # display off, cursor off, cursor blink on
+        self.display.lcd._lcd_write(0b1100,False)
         self.display.lcd.write(64, self.entered_text[self.display_offset:self.display_offset+16].ljust(16))
         self.display.lcd._lcd_write(0xC0 | screen_cursor_pos, False)
-        self.display.lcd._lcd_write(0b1111,False)
+        # display on, cursor on, cursor blink on
+        self.display.lcd._lcd_write(0b1110,False)
 
 
 
